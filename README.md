eabd7cb4daca9d25f172035e76e68780cf4f4dd9
David Blume <david.blume@gmail.com>
2020-02-09 16:36:50 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,66 @@
+[![License](https://img.shields.io/badge/license-MIT_license-blue.svg)](https://raw.githubusercontent.com/dblume/watch-url/master/LICENSE.txt)
+![python3.x](https://img.shields.io/badge/python-3.x-green.svg)
+
+## Receive a notification when a URL changes
+
+With this script you can receive a notification when a URL changes.
+
+## Getting Started
+
+1. Rename watch\_url.json.sample to watch\_url.json.
+2. Customize the notification command in watch\_url.json. (More on this below.)
+3. Launch the script into the background, and disown it.
+
+You can specify an output file for logs with the -o flag, like so:
+
+    $ ./watch_url.py -o watch_url.log https://livejournal.com/ &
+    [1] 12345
+    $ disown 12345
+
+It'll send a test notification after about 10 seconds just so you can see that
+it's working. The next notifications it sends will be because something went wrong
+or the site changed.
+
+## Example Logs
+
+    2020-02-09 15:56:54 INFO PID=14324 Started.
+    2020-02-09 15:56:54 INFO ETag="31c-4ee12cb17cfc0" last_modified=Sat, 21 Dec 2013 22:19:51 GMT
+    2020-02-09 15:57:05 INFO Sending a notification that we're running.
+    2020-02-09 15:58:09 INFO Sending notification of site change.
+    2020-02-09 15:58:09 INFO Exiting. Duration = 75s.
+
+## Example Notification
+
+This notification was sent by email to a phone company that converts email to SMS.
+
+    FRM: user@domainwithemail.com
+    SUBJ: Site changed
+    MSG: https://livejournal.com/
+         From watch_url.py
+
+## Customizing watch\_url.json
+
+The configuration file looks like this:
+
+    {
+       "notification":[
+          "ssh",
+          "user@domainwithemail.com",
+          "printf 'URL\\nFrom watch_url.py' | mail -s 'MSG' 5555550100@txt.phoneco.net"
+       ]
+    }
+
+The one key, "notification", points to a list that can be passed to Python's
+subprocess.run(). In this example, it sends a "mail" command via ssh to a system
+that can email an account at an address that converts email to text.
+
+## Is it any good?
+
+[Yes](https://news.ycombinator.com/item?id=3067434). Especially because it's polite.
+It sends `If-Modified-Since` headers when it detects `Last-Modified` headers, and
+it sends `If-None-Match` headers when it detects `ETag` headers. It then gracefully
+handles the 304 "Not Modified" response code.
+
+## Licence
+
+This software uses the [MIT license](https://raw.githubusercontent.com/dblume/watch-url/master/LICENSE.txt)
---
+++ watch_url.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+import os
+import sys
+import json
+import subprocess
+from argparse import ArgumentParser
+import logging
+import time
+import signal
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
+import hashlib
+
+
+_notification = ['echo', 'URL MSG']
+
+
+def log_exit(sig, frame):
+    """Notify the user when a signal terminates the process."""
+    logging.warning(f"PID={os.getpid()} signal={signal.Signals(sig).name} Exiting.")
+    notify('Watcher exiting.')
+    sys.exit(0)
+
+
+def get_md5(f):
+    """Make an MD5 hash of the page's contents."""
+    BLOCKSIZE = 65536
+    hasher = hashlib.md5()
+    buf = f.read(BLOCKSIZE)
+    while len(buf) > 0:
+        hasher.update(buf)
+        buf = f.read(BLOCKSIZE)
+    return hasher.hexdigest()
+
+
+def notify(msg, url=''):
+    """Send a notification to the user."""
+    return run([i.replace('MSG', msg).replace('URL', url) for i in _notification])
+
+
+def run(command_list):
+    """Pass in a linux command, get back the stdout."""
+    r = subprocess.run(command_list, stdout=subprocess.PIPE,
+                       stderr=subprocess.PIPE, encoding='utf-8')
+    logging.debug(f"subprocess.run({command_list}) got {r.returncode}.")
+    if r.returncode != 0:
+        logging.error(f"subprocess.run({command_list}) failed with code {r.returncode}.")
+        return f"Error {r.returncode} trying to run({command_list})"
+    return r.stdout
+
+
+def main(urls):
+    """The main function, does the whole thing."""
+    global _notification
+    start_time = time.time()
+    with open(__file__.replace('.py', '.json')) as f:
+       _notification = json.load(f)['notification']
+
+    signal.signal(signal.SIGINT, log_exit)
+    signal.signal(signal.SIGTERM, log_exit)
+
+    logging.info(f'PID={os.getpid()} Started.')
+
+    url = urls[0]  # Just for now.
+    if len(urls) > 1:
+        logging.error(f"I haven't implemented support for multiple URLs yet.")
+        return
+
+    # Get ETag and/or Last-Modified, if there is one.
+    req = Request(url)
+    with urlopen(req) as f:
+        if f.getcode() != 200:
+            logging.error(f"Got {f.getcode()} for {url}. Exiting.")
+            notify(f"Got {f.getcode()} for {url}. Exiting.")
+            return
+        if 'ETag' in f.headers:
+            req.add_header('If-None-Match', f.headers['ETag'])
+            etag = f.headers['ETag']
+        else:
+            etag = None
+        if 'Last-Modified' in f.headers:
+            req.add_header('If-Modified-Since', f.headers['Last-Modified'])
+            last_modified = f.headers['Last-Modified']
+        else:
+            last_modified = None
+        md5 = get_md5(f)
+
+    logging.debug(f"ETag={etag} last_modified={last_modified}")
+
+    done = False
+    send_confirmation_at = time.time() + 10  # seconds
+    while not done:
+        time.sleep(5)
+        if send_confirmation_at is not None and send_confirmation_at < time.time():
+            logging.info(f"Sending a notification that we're running.")
+            notify("Watching", url)
+            send_confirmation_at = None
+        try:
+            with urlopen(req) as f:
+                if f.getcode() != 200:
+                    # Maybe these are all covered by exceptions?
+                    logging.error(f"Got {f.getcode()} for {url}. Continuing.")
+                    continue
+                changed = False
+                if 'ETag' in f.headers and f.headers['ETag'] != etag:
+                    changed = True
+                if 'Last-Modified' in f.headers and f.headers['Last-Modified'] != last_modified:
+                    changed = True
+                if not changed and md5 != get_md5(f):
+                    changed = True
+                if changed:
+                    notify("Site changed", url)
+                    logging.info(f"Sending notification of site change.")
+                    done = True
+                last_modified = f.headers['Last-Modified']
+        except HTTPError as e:
+            if e.code == 304:
+                logging.debug(f"Url not changed.")
+            else:
+                logging.error(f"Got {e.code()} for {url}. Continuing.")
+
+    logging.info(f"Exiting. Duration = {time.time() - start_time:2.0f}s.")
+    notify(f"Exiting.")
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description="Notify when a URL changes.")
+    parser.add_argument('-o', '--outfile')
+    parser.add_argument('urls', nargs='+', help='URLs to watch')
+    args = parser.parse_args()
+    if args.outfile is None:
+        handler = logging.StreamHandler(sys.stdout)
+    else:
+        handler = logging.FileHandler(args.outfile)
+    logging.basicConfig(handlers=(handler,),
+                        format='%(asctime)s %(levelname)s %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        level=logging.INFO)
+    main(args.urls)
